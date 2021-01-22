107289c24320613fcb4617276d5e89b4323bbbe8
Wyatt <wyatthunterallan@gmail.com>
2021-01-22 14:50:47 -0700

    New class added to change margin-top of bodyDiv to allow space for small-nav

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -2,12 +2,15 @@
 function toggleMenu() {
   var y = document.getElementById('primaryNav');
   var x = document.getElementById('hamburgerBtn');
+  var bodyDiv = document.getElementsByClassName('bodyDiv');
 if(document.getElementById('primaryNav').className==='closed') {
       y.className='open';
       x.className='open';
+      bodyDiv[0].classList.add('openBody')
     } else {
       y.className='closed';
       x.className='closed'
+      bodyDiv[0].classList.remove('openBody')
     }
 }
 var x = document.getElementById('hamburgerBtn');
