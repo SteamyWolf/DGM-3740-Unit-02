43077848d398a7712144b85845afacaff600f5b3
Wyatt <wyatthunterallan@gmail.com>
2021-03-30 09:11:52 -0600

    changes to serviceArea

--- _serviceArea.php
+++ _serviceArea.php
@@ -8,53 +8,29 @@
 <h1 class="h1-gallery">Service Area</h1>
 
 <div class="serviceAreaFlex">
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea1.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea1.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea2.jpeg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea2.jpeg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea3.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea3.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea4.jpeg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea4.jpeg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea5.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea5.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea6.png" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea6.png" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea7.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea7.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea8.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea8.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea9.jpeg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea9.jpeg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea10.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea10.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea11.jpg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea11.jpg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 
-    <figure>
-        <img src="/serviceAreaPhotos/serviceArea12.jpeg" loading="lazy" alt="cruise photo">
-    </figure>
+        <img src="/serviceAreaPhotos/serviceArea12.jpeg" width="1200px" height="600px" loading="lazy" alt="cruise photo">
 </div>
 
 <?php  include '_footer.php'; ?>
